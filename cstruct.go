a5f189d5463ab11c671581cbbcde26bf509a482a
fananchong <fananchong@126.com>
2018-03-29 16:39:31 +0800

    Update cstruct.go

--- cstruct.go
+++ cstruct.go
@@ -74,6 +74,102 @@ func packField(field *structs.Field, order ByteOrder) []byte {
 				} else {
 					ret = append(ret, c.Bool.PackBE(field.Value().(bool))...)
 				}
+			case CTypeInt8:
+				if order == LE {
+					ret = append(ret, c.Int8.PackLE(field.Value().(int8))...)
+				} else {
+					ret = append(ret, c.Int8.PackBE(field.Value().(int8))...)
+				}
+			case CTypeInt16:
+				if order == LE {
+					ret = append(ret, c.Int16.PackLE(field.Value().(int16))...)
+				} else {
+					ret = append(ret, c.Int16.PackBE(field.Value().(int16))...)
+				}
+			case CTypeInt24:
+				if order == LE {
+					ret = append(ret, c.Int24.PackLE(field.Value().(int32))...)
+				} else {
+					ret = append(ret, c.Int24.PackBE(field.Value().(int32))...)
+				}
+			case CTypeInt32:
+				if order == LE {
+					ret = append(ret, c.Int32.PackLE(field.Value().(int32))...)
+				} else {
+					ret = append(ret, c.Int32.PackBE(field.Value().(int32))...)
+				}
+			case CTypeInt40:
+				if order == LE {
+					ret = append(ret, c.Int40.PackLE(field.Value().(int64))...)
+				} else {
+					ret = append(ret, c.Int40.PackBE(field.Value().(int64))...)
+				}
+			case CTypeInt64:
+				if order == LE {
+					ret = append(ret, c.Int64.PackLE(field.Value().(int64))...)
+				} else {
+					ret = append(ret, c.Int64.PackBE(field.Value().(int64))...)
+				}
+			case CTypeUInt8:
+				if order == LE {
+					ret = append(ret, c.UInt8.PackLE(field.Value().(uint8))...)
+				} else {
+					ret = append(ret, c.UInt8.PackBE(field.Value().(uint8))...)
+				}
+			case CTypeUInt16:
+				if order == LE {
+					ret = append(ret, c.UInt16.PackLE(field.Value().(uint16))...)
+				} else {
+					ret = append(ret, c.UInt16.PackBE(field.Value().(uint16))...)
+				}
+			case CTypeUInt24:
+				if order == LE {
+					ret = append(ret, c.UInt24.PackLE(field.Value().(uint32))...)
+				} else {
+					ret = append(ret, c.UInt24.PackBE(field.Value().(uint32))...)
+				}
+			case CTypeUInt32:
+				if order == LE {
+					ret = append(ret, c.UInt32.PackLE(field.Value().(uint32))...)
+				} else {
+					ret = append(ret, c.UInt32.PackBE(field.Value().(uint32))...)
+				}
+			case CTypeUInt40:
+				if order == LE {
+					ret = append(ret, c.UInt40.PackLE(field.Value().(uint64))...)
+				} else {
+					ret = append(ret, c.UInt40.PackBE(field.Value().(uint64))...)
+				}
+			case CTypeUInt64:
+				if order == LE {
+					ret = append(ret, c.UInt64.PackLE(field.Value().(uint64))...)
+				} else {
+					ret = append(ret, c.UInt64.PackBE(field.Value().(uint64))...)
+				}
+			case CTypeFloat:
+				if order == LE {
+					ret = append(ret, c.Float.PackLE(field.Value().(float32))...)
+				} else {
+					ret = append(ret, c.Float.PackBE(field.Value().(float32))...)
+				}
+			case CTypeDouble:
+				if order == LE {
+					ret = append(ret, c.Double.PackLE(field.Value().(float64))...)
+				} else {
+					ret = append(ret, c.Double.PackBE(field.Value().(float64))...)
+				}
+			case CTypeString:
+				if order == LE {
+					ret = append(ret, c.String.PackLE(field.Value().(string))...)
+				} else {
+					ret = append(ret, c.String.PackBE(field.Value().(string))...)
+				}
+			case CTypeBinary:
+				if order == LE {
+					ret = append(ret, c.Binary.PackLE(field.Value().([]byte))...)
+				} else {
+					ret = append(ret, c.Binary.PackBE(field.Value().([]byte))...)
+				}
 			default:
 				panic(fmt.Sprintf("unknow type! type = %s", ctype))
 			}
@@ -121,6 +217,120 @@ func unpackField(field *structs.Field, buf []byte, pos *int, order ByteOrder) er
 					field.Set(c.Bool.UnpackBE(buf[*pos:]))
 				}
 				*pos = *pos + c.Bool.Size()
+			case CTypeInt8:
+				if order == LE {
+					field.Set(c.Int8.UnpackLE(buf[*pos:]))
+				} else {
+					field.Set(c.Int8.UnpackBE(buf[*pos:]))
+				}
+				*pos = *pos + c.Int8.Size()
+			case CTypeInt16:
+				if order == LE {
+					field.Set(c.Int16.UnpackLE(buf[*pos:]))
+				} else {
+					field.Set(c.Int16.UnpackBE(buf[*pos:]))
+				}
+				*pos = *pos + c.Int16.Size()
+			case CTypeInt24:
+				if order == LE {
+					field.Set(c.Int24.UnpackLE(buf[*pos:]))
+				} else {
+					field.Set(c.Int24.UnpackBE(buf[*pos:]))
+				}
+				*pos = *pos + c.Int24.Size()
+			case CTypeInt32:
+				if order == LE {
+					field.Set(c.Int32.UnpackLE(buf[*pos:]))
+				} else {
+					field.Set(c.Int32.UnpackBE(buf[*pos:]))
+				}
+				*pos = *pos + c.Int32.Size()
+			case CTypeInt40:
+				if order == LE {
+					field.Set(c.Int40.UnpackLE(buf[*pos:]))
+				} else {
+					field.Set(c.Int40.UnpackBE(buf[*pos:]))
+				}
+				*pos = *pos + c.Int40.Size()
+			case CTypeInt64:
+				if order == LE {
+					field.Set(c.Int64.UnpackLE(buf[*pos:]))
+				} else {
+					field.Set(c.Int64.UnpackBE(buf[*pos:]))
+				}
+				*pos = *pos + c.Int64.Size()
+			case CTypeUInt8:
+				if order == LE {
+					field.Set(c.UInt8.UnpackLE(buf[*pos:]))
+				} else {
+					field.Set(c.UInt8.UnpackBE(buf[*pos:]))
+				}
+				*pos = *pos + c.UInt8.Size()
+			case CTypeUInt16:
+				if order == LE {
+					field.Set(c.UInt16.UnpackLE(buf[*pos:]))
+				} else {
+					field.Set(c.UInt16.UnpackBE(buf[*pos:]))
+				}
+				*pos = *pos + c.UInt16.Size()
+			case CTypeUInt24:
+				if order == LE {
+					field.Set(c.UInt24.UnpackLE(buf[*pos:]))
+				} else {
+					field.Set(c.UInt24.UnpackBE(buf[*pos:]))
+				}
+				*pos = *pos + c.UInt24.Size()
+			case CTypeUInt32:
+				if order == LE {
+					field.Set(c.UInt32.UnpackLE(buf[*pos:]))
+				} else {
+					field.Set(c.UInt32.UnpackBE(buf[*pos:]))
+				}
+				*pos = *pos + c.UInt32.Size()
+			case CTypeUInt40:
+				if order == LE {
+					field.Set(c.UInt40.UnpackLE(buf[*pos:]))
+				} else {
+					field.Set(c.UInt40.UnpackBE(buf[*pos:]))
+				}
+				*pos = *pos + c.UInt40.Size()
+			case CTypeUInt64:
+				if order == LE {
+					field.Set(c.UInt64.UnpackLE(buf[*pos:]))
+				} else {
+					field.Set(c.UInt64.UnpackBE(buf[*pos:]))
+				}
+				*pos = *pos + c.UInt64.Size()
+			case CTypeFloat:
+				if order == LE {
+					field.Set(c.Float.UnpackLE(buf[*pos:]))
+				} else {
+					field.Set(c.Float.UnpackBE(buf[*pos:]))
+				}
+				*pos = *pos + c.Float.Size()
+			case CTypeDouble:
+				if order == LE {
+					field.Set(c.Double.UnpackLE(buf[*pos:]))
+				} else {
+					field.Set(c.Double.UnpackBE(buf[*pos:]))
+				}
+				*pos = *pos + c.Double.Size()
+			case CTypeString:
+				if order == LE {
+					field.Set(c.String.UnpackLE(buf[*pos:]))
+					*pos = *pos + c.String.SizeLE(buf[*pos:])
+				} else {
+					field.Set(c.String.UnpackBE(buf[*pos:]))
+					*pos = *pos + c.String.SizeBE(buf[*pos:])
+				}
+			case CTypeBinary:
+				if order == LE {
+					field.Set(c.Binary.UnpackLE(buf[*pos:]))
+					*pos = *pos + c.Binary.SizeLE(buf[*pos:])
+				} else {
+					field.Set(c.Binary.UnpackBE(buf[*pos:]))
+					*pos = *pos + c.Binary.SizeBE(buf[*pos:])
+				}
 			default:
 				return errors.New(fmt.Sprintf("unknow type! type = %s", ctype))
 			}
--- tests/example1/example1.go
+++ tests/example1/example1.go
@@ -6,21 +6,75 @@ import (
 	cstruct "github.com/fananchong/cstruct-go"
 )
 
+type mystruct3 struct {
+	F3 float64 `c:double`
+	F4 string  `c:string`
+}
+
+type mystruct2 struct {
+	F6  int8  `c:int8`
+	F7  int16 `c:int16`
+	F8  int32 `c:int24`
+	F9  int32 `c:int32`
+	F10 int64 `c:int40`
+	F11 int64 `c:int64`
+	S0  mystruct3
+}
+
 type mystruct1 struct {
-	F0 bool `c:"bool"`
-	//	F1 int8      `c:int8`
-	//	F2 float32   `c:float`
-	//	F3 float64   `c:double`
-	//	F4 string    `c:string`
-	//	F5 []byte    `c:binary`
-	//	F6 mystruct2 `c:struct`
+	N0  uint32
+	F1  bool    `c:"bool"`
+	F2  float32 `c:"float"`
+	F3  float64 `c:"double"`
+	S0  mystruct2
+	F4  string `c:"string"`
+	F5  []byte `c:"binary"`
+	F6  int8   `c:"int8"`
+	F7  int16  `c:"int16"`
+	F8  int32  `c:"int24"`
+	F9  int32  `c:"int32"`
+	F10 int64  `c:"int40"`
+	F11 int64  `c:"int64"`
+	N1  float32
+	F12 uint8  `c:"uint8"`
+	F13 uint16 `c:"uint16"`
+	F14 uint32 `c:"uint24"`
+	F15 uint32 `c:"uint32"`
+	F16 uint64 `c:"uint40"`
+	F17 uint64 `c:"uint64"`
+	N2  bool
 }
 
 func main() {
 
 	// le
 	a := &mystruct1{}
-	a.F0 = true
+	a.F1 = true
+	a.F2 = 0.98
+	a.F3 = 999.777
+	a.F4 = "hello"
+	a.F5 = []byte{1, 2, 3, 4}
+	a.F6 = 126
+	a.F7 = 32766
+	a.F8 = -8388608
+	a.F9 = -2147483646
+	a.F10 = 549755813887
+	a.F11 = -9223372036854774807
+	a.F12 = 254
+	a.F13 = 65534
+	a.F14 = 16777214
+	a.F15 = 4294967295
+	a.F16 = 1099511627770
+	a.F17 = 18446744073709551614
+	a.S0.F6 = -126
+	a.S0.F7 = -32766
+	a.S0.F8 = 8388600
+	a.S0.F9 = 2147483640
+	a.S0.F10 = -549755813880
+	a.S0.F11 = 9223372036854774800
+	a.S0.S0.F3 = 100.9
+	a.S0.S0.F4 = "world"
+
 	buf_l := cstruct.PackLE(a)
 	fmt.Println("Buf(l):", buf_l)
 	b := &mystruct1{}
@@ -31,9 +85,7 @@ func main() {
 	fmt.Println("NewVal(l):", *b)
 
 	// be
-	c := &mystruct1{}
-	c.F0 = false
-	buf_b := cstruct.PackBE(c)
+	buf_b := cstruct.PackBE(a)
 	fmt.Println("Buf(b):", buf_b)
 	d := &mystruct1{}
 	if err := cstruct.UnpackBE(buf_b, d); err != nil {
